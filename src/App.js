c5e2468c00822e76ba1e5a55ce9728a9570f8f9f
klausdw <klausdw@outlook.com>
2020-12-21 18:21:56 +0100

    sing-in

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.jsx';
-import ShopPage from './pages/shop/shop.jsx';
-import Header from './components/header/header.jsx'
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import Header from './components/header/header'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>  
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>  
   );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.style.scss';
+import './header.styles.scss';
 
 const Header = () => (
     <div className='header'>
