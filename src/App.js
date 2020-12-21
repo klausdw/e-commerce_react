8e8594ae72c367fae05b1889e5b08ad6d7a90bbb
klausdw <klausdw@outlook.com>
2020-12-21 17:28:46 +0100

    header, links:home, shop, contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop';
+import HomePage from './pages/homepage/homepage.jsx';
+import ShopPage from './pages/shop/shop.jsx';
+import Header from './components/header/header.jsx'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>  
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
