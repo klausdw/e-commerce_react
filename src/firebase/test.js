68b7cbeb7005f6367d77f32ad7df830fb100e593
klausdw <klausdw@outlook.com>
2020-12-22 10:54:04 +0100

    firestore

---
+++ src/firebase/test.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('LSz4vvNwSjTNOs5xftFv').collection('cartItems').doc('0IjpYNZBYkoMZeCIus3o')
+firestore.doc('/users/LSz4vvNwSjTNOs5xftFv/cartItems/0IjpYNZBYkoMZeCIus3o');
+firestore.collection('/users/LSz4vvNwSjTNOs5xftFv/cartItems');
