7f300fb936554b375d67525f99dad8854d2b8cda
klausdw <klausdw@outlook.com>
2020-12-27 18:54:42 +0100

    cart-item

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -28,7 +30,9 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             )}
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 );
 
