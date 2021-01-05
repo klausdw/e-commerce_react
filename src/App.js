050ecff5d96e09f3f51022a2568e8baca0f934fb
klausdw <klausdw@outlook.com>
2021-01-05 15:29:55 +0100

    Moving Shop Data to Firebse, manually

--- src/App.js
+++ src/App.js
@@ -12,17 +12,18 @@ import CheckoutPage from './pages/checkout/checkout';
 
 import Header from './components/header/header'
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selector';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -37,6 +38,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+      addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({ title, items})));
     });
   }
 
@@ -70,7 +72,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector ({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -40,6 +40,24 @@ const config = {
 
   firebase.initializeApp(config);
 
+export const addCollectionAndDocuments = async (
+  collectionKey, 
+  objectsToAdd
+  ) => {
+  const collectionRef = firestore.collection(collectionKey);
+  //console.log(collectionRef);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    //const newDocRef = collectionRef.doc(obj.title);
+    const newDocRef = collectionRef.doc();
+    //console.log(newDocRef)
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
